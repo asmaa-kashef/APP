971a29b71e12e42a340118aa49a980e1ffc7fd61
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-07-08 22:15:08 +0200

    Add files via upload

--- Application/src/main/java/Main.java
+++ Application/src/main/java/Main.java
@@ -22,15 +22,12 @@ public class Main {
         
         String File_Name="C:\\Users\\asmaa\\OneDrive\\Desktop\\iti\\java\\Day4\\dataset\\Cities.csv";
         SortedCities sc= new SortedCities();
-        SortedCitesStream St=new SortedCitesStream();
-        List<CitiesPojo>Cities_matched= St.Filter(File_Name,"EGY");
          LinkedList<CitiesPojo>Cities_filtered=sc.Filter(File_Name, "EGY");
          System.out.println(Cities_filtered);
-         System.out.println(Cities_matched);
-//         HashMap<String, LinkedList<CitiesPojo>>CitiesMap= sc.createdMap(Cities_filtered);
-//         System.out.println(CitiesMap);
-//          LinkedList<CitiesPojo> sortedCity=sc.sortedCity(CitiesMap);
-//          System.out.println(sortedCity);
+         HashMap<String, LinkedList<CitiesPojo>>CitiesMap= sc.createdMap(Cities_filtered);
+         System.out.println(CitiesMap);
+          LinkedList<CitiesPojo> sortedCity=sc.sortedCity(CitiesMap);
+          System.out.println(sortedCity);
         
         
         // TODO code application logic here
