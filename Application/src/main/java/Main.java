cd899c6e502b3ccb5c0e0b9f9ed1b54087d8c0e5
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-07-08 22:28:34 +0200

    Update Main.java

--- Application/src/main/java/Main.java
+++ Application/src/main/java/Main.java
@@ -20,14 +20,18 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        String File_Name="C:\\Users\\asmaa\\OneDrive\\Desktop\\iti\\java\\Day4\\dataset\\Cities.csv";
-        SortedCities sc= new SortedCities();
-         LinkedList<CitiesPojo>Cities_filtered=sc.Filter(File_Name, "EGY");
-         System.out.println(Cities_filtered);
-         HashMap<String, LinkedList<CitiesPojo>>CitiesMap= sc.createdMap(Cities_filtered);
-         System.out.println(CitiesMap);
-          LinkedList<CitiesPojo> sortedCity=sc.sortedCity(CitiesMap);
-          System.out.println(sortedCity);
+          String File_Name="C:\\Users\\asmaa\\OneDrive\\Desktop\\iti\\java\\Day4\\dataset\\Cities.csv";
+        String Countries="C:\\Users\\asmaa\\OneDrive\\Desktop\\iti\\java\\Day4\\dataset\\Countries.csv";
+        SortedCitesStream sc= new SortedCitesStream();
+        List<CitiesPojo> MatchedCityes=sc.Filter(File_Name, "EGY");
+         System.out.println(MatchedCityes);
+          List<CitiesPojo> SortCiti  =sc.SortCities(MatchedCityes);
+          System.out.println(SortCiti);
+          OptionalInt highSortCit=sc.highSortCities(SortCiti);
+          System.out.println(highSortCit);
+          List<Double> listOfCoPopulation=sc.listOfCountriesPopulation(Countries);
+           System.out.println(listOfCoPopulation);
+        // TODO code application logic here
         
         
         // TODO code application logic here
